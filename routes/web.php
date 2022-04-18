0e555ef5d7bd318dc34f341a649ed2322d35a96a
alamgirweb11 <alamgirweb11@gmail.com>
2022-04-18 22:52:54 +0600

    add post model migration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -25,6 +26,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
+    Route::resource('posts', PostController::class);
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
